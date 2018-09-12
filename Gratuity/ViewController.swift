eb3cc9e494ee7dbbc1842e2178acb53af750a6bd
yan lawrence <YanLawrence@Yans-MacBook-Pro.local>
2018-09-12 13:08:15 -0400

    added.gitignore

--- Gratuity/ViewController.swift
+++ Gratuity/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,21 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTIp(_ sender: Any) {
+        
+        let tipPercentage = [0.15, 0.18, 0.20, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage [tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
 }
 
